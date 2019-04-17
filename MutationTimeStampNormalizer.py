2fd6f66aba1d5c2a7a5432e767361d9af6463ac3
piksu <jouni.pikkarainen@gmail.com>
2019-04-17 17:18:51 +0300

    Fixed MutationTimeStampNormalizer

--- MutationTimeStampNormalizer.py
+++ MutationTimeStampNormalizer.py
@@ -2,11 +2,11 @@ from LogFileSanitizer import LogFileSanitizer
 
 
 class MutationTimeStampNormalizer(LogFileSanitizer):
-    """MutationTimeStampNormalizer normalizes LogFile mutations timestamps todo
-        to start from 0.
+    """MutationTimeStampNormalizer normalizes LogFile mutations timestamps
+        to start from "0".
 
         Simply takes the first line, assing the timestamps of it to zero and
-        saves the original timestmap. The original timestamp is used to
+        saves the original timestamp. The original timestamp is used to
         readjust the rest of the timestamps for the particular mutation.
     """
 
@@ -15,9 +15,9 @@ class MutationTimeStampNormalizer(LogFileSanitizer):
 
     def sanitize(self, LogFile):
         for mi, mutation in enumerate(LogFile.mutations):
-            for i, line in enumerate(mutation.data):
+            for i, (timestamp, event) in enumerate(mutation.data):
                 if i == 0:
-                    firstLineTimeStamp = mutation.data[i][0]
-                    LogFile.mutations[mi].data[i][0] = '%g' % (float(LogFile.mutations[mi].data[i][0].replace(
-                        ',', '.')) - float(firstLineTimeStamp.replace(',', '.')))
+                    mutation_timestamp_delta = timestamp
+                LogFile.mutations[mi].data[i][0] = '%g' % (float(LogFile.mutations[mi].data[i][0].replace(
+                        ',', '.')) - float(mutation_timestamp_delta.replace(',', '.')))
         return LogFile
